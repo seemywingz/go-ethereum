a3905460f61be15e9d6b7d6ce8d224c5b3659af7
Kevin Jayne <kevin.jayne@icloud.com>
2018-03-31 21:07:03 -0400

    single node with bootnode working

--- private-net/scripts/gethLoad.js
+++ private-net/scripts/gethLoad.js
@@ -1,6 +1,6 @@
 // loadScript("/private-net/scripts/gethLoad.js")
 
-function allBalances() {
+function all() {
   var totalBal = 0;
   for (var acctNum in eth.accounts) {
       var acct = eth.accounts[acctNum];
@@ -10,3 +10,7 @@ function allBalances() {
   }
   console.log("  Total balance: " + totalBal + " ether");
 };
+
+function send() {
+  eth.sendTransaction({from:"0xa1640b88f612be6c2031ca07f0f4862dae4a3fae", to:"0xbebe38ec6ef2b5f3b56bfebb353ebc860e382155", value:900})
+}
--- private-net/scripts/notes.js
+++ private-net/scripts/notes.js
@@ -1,13 +1,12 @@
-eth.sendTransaction({from:eth.accounts[0], to:"0x4f29512243576EaFe5C2122dFA73B08B30D28D1f", value:20})
+loadScript("/private-net/scripts/gethLoad.js")
 
 
-eth.sendTransaction({from:"0xbebe38ec6ef2b5f3b56bfebb353ebc860e382155", to:"0xb106dd9b8423b75787a4542c6dd183c468bf2442", value:9})
-eth.getBalance("0xb106dd9b8423b75787a4542c6dd183c468bf2442")
-
-
-eth.getBalance(eth.coinbase)
+web3.fromWei(eth.getBalance("0x3cd1db5f09b51c56c266989aae66a61a13064039"), "ether")
+web3.fromWei(eth.getBalance("0xa1640b88f612be6c2031ca07f0f4862dae4a3fae"), "ether")
+web3.fromWei(eth.getBalance("0xbebe38ec6ef2b5f3b56bfebb353ebc860e382155"), "ether")
 
 web3.fromWei(eth.getBalance(eth.coinbase), "ether")
-web3.fromWei(eth.getBalance(eth.accounts[1]), "ether")
 
-loadScript("/private-net/scripts/gethLoad.js")
+eth.sendTransaction({from:'0xbebe38ec6ef2b5f3b56bfebb353ebc860e382155', to:'0xa1640b88f612be6c2031ca07f0f4862dae4a3fae', value: web3.toWei(0.05, "ether"), gas:21000});
+
+web3.eth.getTransactionReceipt("receiptHash")
