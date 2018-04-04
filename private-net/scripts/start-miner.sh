20f1ebd36449a8c744595d1cf58d53325b5222fe
Kevin Jayne <kevin.jayne@icloud.com>
2018-04-03 20:13:25 -0400

    keep geth commands in compose file

--- private-net/scripts/start-miner.sh
+++ private-net/scripts/start-miner.sh
@@ -4,10 +4,11 @@ geth \
   --networkid $NETWORK_ID \
   --password /private-net/files/password.txt \
   --unlock $UNLOCKED_ACCOUNTS \
-  --nodiscover \
   --rpc \
   --rpccorsdomain "*" \
   --rpcaddr 0.0.0.0 \
   --rpcapi="eth,net,web3,personal" \
-  --verbosity 9 \
-  --nodekeyhex $BOOTNODE_KEY_HEX 
+  --mine \
+  --minerthreads=1 \
+  --etherbase $ETHERBASE \
+  --nodekeyhex $MINER_KEY_HEX 
