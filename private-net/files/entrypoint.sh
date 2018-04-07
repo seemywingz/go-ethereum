389fced6f06fa00116e709f69736e9d9b5e36d4b
Kevin Jayne <kevin.jayne@icloud.com>
2018-04-07 01:30:31 -0400

    compose

--- private-net/files/entrypoint.sh
+++ private-net/files/entrypoint.sh
@@ -2,9 +2,8 @@
 
 geth \
   --networkid $NETWORK_ID \
-  --password /private-net/files/password.txt \
-  --unlock $UNLOCKED_ACCOUNTS \
   --rpc \
   --rpccorsdomain "*" \
   --rpcaddr 0.0.0.0 \
-  --rpcapi="eth,net,web3,personal" "${@}"
+  --rpcapi="eth,net,web3,personal" \
+  "${@}"
